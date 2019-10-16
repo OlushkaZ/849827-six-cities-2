de4034370db610a8d618738a6fc8d0ed02a11b67
OlushkaZ <gincgo@yandex.ru>
2019-10-16 21:32:21 +0300

    start

---
+++ src/components/app/app.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {PageMain} from '../page-main/page-main.jsx';
+export const App = () => {
+  // const {gameTime, errorCount} = props;
+
+  return <PageMain
+    // time={gameTime}
+    // errorCount={errorCount}
+  />;
+};
