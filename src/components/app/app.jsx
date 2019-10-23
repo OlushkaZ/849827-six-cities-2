997dfe0030ca9ee1a6b2de0c90111bad5a479933
OlushkaZ <gincgo@yandex.ru>
2019-10-23 22:48:34 +0300

    placeCard

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 import PageMain from '../page-main/page-main.jsx';
 import PropTypes from "prop-types";
-const App = ({offers, onClick}) => {
+const App = ({offers}) => {
 
   return <PageMain
     offers={offers}
-    onClick={onClick}
+    // onClick={onClick}
   />;
 };
 App.propTypes = {
-  offers: PropTypes.arrayOf(PropTypes.string),
-  onClick: ()=>{},
+  offers: PropTypes.arrayOf(
+      PropTypes.exact({
+        title: PropTypes.string,
+        coast: PropTypes.number,
+        isPremium: PropTypes.bool,
+        type: PropTypes.string,
+        src: PropTypes.string,
+      })
+  )
+  // onClick: ()=>{},
 };
 export default App;
--- src/components/page-main/page-main.jsx
+++ src/components/page-main/page-main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
-const PageMain = ({offers, onClick}) => {
+import PlaceList from '../place-list/place-list.jsx';
+const PageMain = ({offers}) => {
 
   return <div className="page page--gray page--main">
     <header className="header">
@@ -92,43 +93,9 @@ const PageMain = ({offers, onClick}) => {
               </select>
             */}
             </form>
-            <div className="cities__places-list places__list tabs__content">
-              {offers.map((title, i)=><article key={title + i} className="cities__place-card place-card">
-                <div className="place-card__mark">
-                  <span>Premium</span>
-                </div>
-                <div className="cities__image-wrapper place-card__image-wrapper">
-                  <a href="#">
-                    <img className="place-card__image" src="img/apartment-01.jpg" width="260" height="200" alt="Place image"/>
-                  </a>
-                </div>
-                <div className="place-card__info">
-                  <div className="place-card__price-wrapper">
-                    <div className="place-card__price">
-                      <b className="place-card__price-value">&euro;120</b>
-                      <span className="place-card__price-text">&#47;&nbsp;night</span>
-                    </div>
-                    <button className="place-card__bookmark-button button" type="button" onClick = {onClick}>
-                      <svg className="place-card__bookmark-icon" width="18" height="19">
-                        <use xlinkHref="#icon-bookmark"></use>
-                      </svg>
-                      <span className="visually-hidden">To bookmarks</span>
-                    </button>
-                  </div>
-                  <div className="place-card__rating rating">
-                    <div className="place-card__stars rating__stars">
-                      <span style={{width: `93%`}}></span>
-                      <span className="visually-hidden">Rating</span>
-                    </div>
-                  </div>
-                  <h2 className="place-card__name">
-                    <a href="#">{title}</a>
-                  </h2>
-                  <p className="place-card__type">Apartment</p>
-                </div>
-              </article>)}
-
-            </div>
+            <PlaceList
+              offers={offers}
+            />
           </section>
           <div className="cities__right-section">
             <section className="cities__map map"></section>
@@ -140,7 +107,15 @@ const PageMain = ({offers, onClick}) => {
 
 };
 PageMain.propTypes = {
-  offers: PropTypes.arrayOf(PropTypes.string),
-  onClick: ()=>{},
+  offers: PropTypes.arrayOf(
+      PropTypes.exact({
+        title: PropTypes.string,
+        coast: PropTypes.number,
+        isPremium: PropTypes.bool,
+        type: PropTypes.string,
+        src: PropTypes.string,
+      })
+  )
+  // onClick: ()=>{},
 };
 export default PageMain;
