261bd2f62be47054cebe3e1ae4552a93dc4d0b1e
OlushkaZ <gincgo@yandex.ru>
2019-11-13 23:00:18 +0300

    connect to server

---
+++ src/configure-api.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+const configureAPI = axios.create({
+  baseURL: `https://htmlacademy-react-2.appspot.com/six-cities`,
+  timeout: 5000,
+  withCredentials: true
+});
+export default configureAPI;
--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -1,38 +1,85 @@
 
 import {chooseOffersByCity} from '../utils.js';
 import Offers from '../mocks/offers.js';
+import configureAPI from '../configure-api';
+
 
 const initialState = {
   currentCity: Offers[0].city,
-  currentOffers: chooseOffersByCity(Offers[0].city, Offers),
+  currentOffers: [
+    {
+      id: `id1`,
+      city: `crumlov`,
+      type: `apartment`,
+      title: `Beautiful & luxurious apartment at great location`,
+      coast: 120,
+      isPremium: true,
+      src: `http://placeimg.com/260/200/arch`,
+      coordinates: [48.806887, 14.308579]
+    }],
+  // currentCity: ``,
+  // currentOffers: [],
+  offers: [],
 };
 
-export const reducer = (state = initialState, action)=>{
+const ActionType = {
+  CHANGE_CITY: `CHANGE_CITY`,
+  GET_OFFERS: `GET_OFFERS`,
+  RESET: `RESET`,
+  LOAD_OFFERS: `LOAD_OFFERS`,
+};
+
+const reducer = (state = initialState, action)=>{
   switch (action.type) {
-    case `CHANGE_CITY`: return Object.assign(
+    case ActionType.CHANGE_CITY: return Object.assign(
         {}, state, {currentCity: action.payload}
     );
-    case `GET_OFFERS`: return Object.assign(
+    case ActionType.GET_OFFERS: return Object.assign(
         {}, state, {currentOffers: action.payload}
     );
-    case `RESET`: return Object.assign(
+    case ActionType.RESET: return Object.assign(
         {}, initialState
+    );
+    case ActionType.LOAD_OFFERS: return Object.assign(
+        {}, state, {offers: action.payload}
     );
   }
   return state;
 };
 
 
-export const ActionCreator = {
+const ActionCreator = {
   changeCity: (city)=>({
-    type: `CHANGE_CITY`,
+    type: ActionType.CHANGE_CITY,
     payload: city
   }),
   getOffers: (offers)=>({
-    type: `GET_OFFERS`,
+    type: ActionType.GET_OFFERS,
     payload: offers
   }),
   reset: ()=>({
-    type: `RESET`
-  })
+    type: ActionType.RESET
+  }),
+  loadOffers: (offers)=>({
+    type: ActionType.LOAD_OFFERS,
+    payload: offers
+  }),
+};
+
+const Operation = {
+  loadOffers: () => (dispatch) => {
+    return configureAPI.get(`/hotels`)
+    .then((response)=>{
+      dispatch(ActionCreator.loadOffers(response.data));
+      dispatch(ActionCreator.changeCity(response.data[0].city.name));
+      dispatch(ActionCreator.getOffers(chooseOffersByCity(response.data[0].city, response.data)));
+    });
+  },
+};
+
+export {
+  reducer,
+  ActionType,
+  ActionCreator,
+  Operation,
 };
